48f3ea2246005b675402f35488895fda80124473
aahmedYassin <45315700+aahmedYassin@users.noreply.github.com>
2022-12-08 20:35:23 +0200

    Add files via upload

---
+++ 1. Two Sum.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+       	
+	int indicies[]=new int[2];
+        
+        for(int i=0;i<nums.length;i++){
+        
+            for(int j=i+1;j<nums.length;j++){
+            
+                if(nums[i]+nums[j]==target){
+                
+                        indicies[0]=i;
+                        
+			indicies[1]=j;
+                       
+			 break;
+             
+                }            
+            
+            }       
+        
+        }
+        
+        return indicies;
+    }
+}
